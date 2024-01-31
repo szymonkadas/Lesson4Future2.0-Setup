720dfb6c3f39896a792d416cc1abe141540c6246
AW2077 <aga.wlodawiec@gmail.com>
2024-01-31 23:59:48 +0100

    feat(navbar): desktop navbar added, mobile version needed

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styles from "./NavBar.module.scss";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.navContainer}>
+        <div className={styles.logoBox}>
+          <a>
+            <img src="src/assets/img/Logo-horyzontalne-kolor-SVG.svg" alt="" />
+          </a>
+        </div>
+        <ol>
+          <a
+            href="http://lesson4future.com"
+            aria-label="Lesson4Future homepage"
+          >
+            <li>Start</li>
+          </a>
+          {/* <!-- <a href=""><li>Nauczyciel</li></a> --> */}
+          <a href="http://lesson4future.com/index.html#about">
+            <li>O nas</li>
+          </a>
+          <a href="http://lesson4future.com/kontakt.html">
+            <li>Kontakt</li>
+          </a>
+        </ol>
+      </div>
+      <div className={styles.buttonsBox}>
+        {/* To do: linki */}
+        <a href="/login">
+          <div className={styles.logInButton}>Zaloguj się</div>
+        </a>
+        <a href="/register">
+          <div className={styles.signUpButton}>Zarejestruj się</div>
+        </a>
+      </div>
+      {/* <input type="checkbox" id="chk" /> */}
+      {/* dlaczego to tu jest? */}
+
+      {/* wersja mobilna */}
+      {/* <div className={styles.responsiveNav}>
+            <button> */}
+      {/* handleShowUp */}
+      {/* <div className={styles.open}><span>Menu</span><i className="fas fa-bars"></i></div>
+            <div className={styles.close}><span>Zamknij</span><i className="fas fa-times"></i></div>
+            </button>
+        </div>
+        <div className={styles.navNakladka}></div>
+        <div className={styles.responsiveNavBox}>
+            <ul>
+            <a href="http://lesson4future.com"><li>Start</li></a> */}
+      {/* <!-- <a href=""><li>Nauczyciel</li></a> --> */}
+      {/* <a href="http://lesson4future.com/index.html#about"><li>O nas</li></a>
+          <a href="http://lesson4future.com/kontakt.html"><li>Kontakt</li></a>
+          <a href="/login"><li className="login">Zaloguj się</li></a>
+          <a href="/register"><li className="sign-up">Zarejestruj się</li></a>
+            </ul>
+        </div> */}
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import Footer from "./components/Footer/Footer.tsx";
+import NavBar from "./components/NavBar/NavBar.tsx";
 import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <NavBar />
     <App />
     <Footer />
   </React.StrictMode>
